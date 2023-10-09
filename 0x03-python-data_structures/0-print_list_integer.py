87012e8635afc7d97aa992dadf6eb71fb18634db
Nii-101 <noelamptey101@gmail.com>
2023-10-09 16:18:11 +0000

    Got rid of the while loop and used a for loop

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
-    a = 0
     x = len(my_list)
-    while a < x:
-        print("{}".format(my_list[a]))
-        a = a + 1
+    for i in range (x):
+        print("{}".format(my_list[i]))
