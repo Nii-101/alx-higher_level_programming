826bc76eec0ae4cc48bc7512c389341bed85be13
Nii-101 <noelamptey101@gmail.com>
2023-10-25 14:29:02 +0000

    Finished with task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''A class that defines a square '''
+
+class Square:
+
+    def __init__(self, size):
+        ''' Initializing a square class
+        Args: the size of the square
+        '''
+        self.__size = size
+
