d266d8b385fdb9128f7ae34ac102465ad03a9b0e
Nii-101 <noelamptey101@gmail.com>
2023-10-30 20:25:56 +0000

    Resolved bugs that caused code fails

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
               height = height of the rectangle;
         """
 
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self, width=0):
