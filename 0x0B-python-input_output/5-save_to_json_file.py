40cf8544586ee71ec794f24f2b2fce9129bc1b87
Nii-101 <noelamptey101@gmail.com>
2023-11-08 15:30:24 +0000

    Finished task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Writes an object to a text file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    x = json.dumps(my_obj)
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(x)
