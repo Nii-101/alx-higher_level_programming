9ecfa87d8cd197043d4015a27e77de6956b70260
Nii-101 <noelamptey101@gmail.com>
2023-11-12 23:11:35 +0000

    Finished with task 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,7 +81,11 @@ class Rectangle(Base):
 
     def display(self):
         """ Prints the rectangle to standard out"""
-        for a in range(self.__height):
-            for b in range(self.__width):
-                print("#", end="")
+        for i in range(self.__y):
             print()
+        for a in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
+            self.id,self.__x, self.__y, self.__width, self.__height)
