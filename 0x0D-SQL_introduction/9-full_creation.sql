42d5ea3b6c9d5b9b2350ae631599d15fd800a60a
Nii-101 <noelamptey101@gmail.com>
2023-12-12 22:53:20 +0000

    Tried fixing codefails

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- Creates a new table with data under the hbtn_0c_0 database
 USE hbtn_0c_0;
 
-CREATE TABLE IF NOT EXISTS second_table (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO second_table (`id`, `name`, `score`)
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score)
 VALUES
 	(1, "John", 10),
 	(2, "Alex", 3),
