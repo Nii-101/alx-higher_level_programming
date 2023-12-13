c9e0ecae456e6b51ddf6807899576c3076ce86c2
Nii-101 <noelamptey101@gmail.com>
2023-12-13 01:02:00 +0000

    Removed inserted commas around month values

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,7 @@
 -- Displays the top 3 city temperatures during July and August
-
 SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
-WHERE `month` = '7' OR '8'
+WHERE `month` = 7 OR 8
 GROUP BY `city`
 ORDER BY `avg_temp` DESC
 LIMIT 3;
