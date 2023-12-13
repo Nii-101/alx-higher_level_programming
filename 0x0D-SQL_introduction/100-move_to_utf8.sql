39f5ea87ae4ae3655412679701310174fc277e21
Nii-101 <noelamptey101@gmail.com>
2023-12-13 00:10:37 +0000

    Checking code fails

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- Converting the database, table(first_table) and field name to utf8
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-
 USE hbtn_0c_0
 ALTER TABLE first_table 
 CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+SELECT CONVERT(name USING utf8) FROM first_table;
