ba31d208d8c26af8deabff787a9139ce46c1d57f
Nii-101 <noelamptey101@gmail.com>
2024-01-09 22:34:21 +0000

    Done with task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+const { argv } = require('process');
+if (argv[2] === undefined) { console.log('undefined is undefined'); } else if (argv[3] === undefined) { console.log(argv[2] + ' is undefined'); } else { console.log(argv[2] + ' is ' + argv[3]); }
