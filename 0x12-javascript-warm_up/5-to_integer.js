d3ee5d16eee7220f2ef7dc84b0f318f44e65f5c6
Nii-101 <noelamptey101@gmail.com>
2024-01-10 12:47:02 +0000

    Done with task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const convertArg = Number(argv[2]);
+if (isNaN(convertArg)) {
+  console.log('Not a number');
+} else {
+  console.log(convertArg);
+}
