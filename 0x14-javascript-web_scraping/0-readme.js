97f664109d7bdac81143105e66c730fddfd19993
Nii-101 <noelamptey101@gmail.com>
2024-02-21 03:46:51 -0800

    Done with task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const argument = process.argv[2];
+
+fs.readFile(argument, 'utf-8', (err,content) => {
+	if (err){
+		console.log(err);
+	} else{
+		console.log(content.toString());
+	}
+	
+
+});
