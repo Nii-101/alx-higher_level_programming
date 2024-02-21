d79e5631d3db29876556420a384127f11aeccd40
Nii-101 <noelamptey101@gmail.com>
2024-02-21 10:50:52 -0800

    Done with task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+const request = require('request');
+const link = process.argv[2];
+const storePath = process.argv[3];
+
+request(link).pipe(fs.createWriteStream(storePath));
